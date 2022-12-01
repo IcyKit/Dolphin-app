7ab05c4cd6b00204ead5d55ef65231bb84aaf3cb
IcyKitMac <icykitdesign@gmail.com>
2022-12-01 17:13:25 +0300

    Update: SQL statement added

--- app.js
+++ app.js
@@ -4,14 +4,35 @@ const { Client } = require("pg");
 
 const dbTest = async () => {
   const client = new Client({
-    host: "localhost",
+    host: "dpg-ce4anoarrk0djk4lds60-a",
     port: 5432,
-    user: "postgres",
-    password: "DrRespect228",
+    user: "icykit",
+    password: "zC5UPHaENAenLMezWnFfiBkmov9PlKXk",
     database: "twitter_development",
   });
   await client.connect();
 
+  const makeTest = await client.query(`CREATE TABLE users (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	nickname VARCHAR(255) NOT NULL,
+	description VARCHAR(255),
+	email varchar(255),
+	totalMessages int,
+	totalFollowers int,
+	totalFollowing int,
+	avatarPhoto varchar(255),
+	headerPhoto varchar(255),
+	location varchar(255),
+	website varchar(255),
+	birthday date
+);`);
+  const addInfo = await client.query(`
+INSERT INTO users (name, nickname, description, email)
+VALUES 
+('Nikita', 'icykit', 'Fullstack Developer', 'icykitdesign@gmail.com'),
+('Ivan', 'itoldstopthecar', 'Car lover', 'itold@gmail.com');
+  `);
   const res = await client.query(`SELECT * FROM users`);
   await client.end();
   return res.rows;
